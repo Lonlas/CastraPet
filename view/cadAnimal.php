9b4fdace76e0901356fb3da8dc63ca7caf1c5ab7
LeugiMH <miguelhenriquegs369@gmail.com>
2022-03-19 19:42:30 -0300

    Link HTML com CSS

--- view/cadAnimal.php
+++ view/cadAnimal.php
@@ -5,6 +5,8 @@
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <title>Cadastro do Animal</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="recursos/css/root.css">
+        <link rel="stylesheet" href="recursos/css/cadAnimal.css">
         <!--
         <style type="text/css">
              @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
