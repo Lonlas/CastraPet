08f7614171a72820b8bfdec28c00545a73e74a63
Nicolas Longo <nicolaslongo18@gmail.com>
2022-03-19 18:54:28 -0300

    Separando CSS das páginas php

--- view/esqSenha.php
+++ view/esqSenha.php
@@ -5,6 +5,7 @@
         <title>Esqueci a Senha</title>
         <!--<meta name="description" content="Descrição do conteúdo da página"/>-->
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <!--
         <style type="text/css">
                 @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
             :root{
@@ -160,6 +161,7 @@
                 }
             }
         </style>
+        -->
     </head>
     <body>
         <form id="caixa" action="<?php echo URL.'inicio'?>" method="POST">
