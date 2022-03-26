e64e40368b9f2adfc96f4e5e0c66786ee0f71172
LeugiMH <miguelhenriquegs369@gmail.com>
2022-03-26 17:49:44 -0300

    Criação da conexão, menu e home início do home

---
+++ model/conexao.php
@@ -0,0 +1,18 @@
+<?php
+class Conexao{
+
+    static function conectar(){
+        //Informações para acessar o servidor do banco de dados
+        $localhost = "mysql:host=localhost;port=3306;dbname=bdcastrapet";
+        $usuario = "root";
+        $senha = "usbw";
+
+        $con = new PDO($localhost,$usuario,$senha);
+            
+        //Ativando recurso de exibição de erro
+        $con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            
+        return $con;//Retorna conexão para uso
+    }
+}
+?>
--- view/menu.php
+++ view/menu.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-  <a class="navbar-brand" href="<?php echo URL.'sobre'; ?>">Início</a>
+<nav class="navbar navbar-expand-md navbar-light bg-transparent" style="padding: 2rem 2rem;">
+  <a class="navbar-brand" href="<?php echo URL.'inicio'; ?>">Início</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,17 +7,17 @@
   <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo URL; ?>">Inicío <span class="sr-only">(página atual)</span></a>
+        <a class="nav-link"  href="<?php echo URL; ?>">Inicío <span class="sr-only">(página atual)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo URL.'sobre'; ?>">Sobre</a>
+        <a class="nav-link"  href="<?php echo URL.'cadastraanimal'; ?>">Cadastrar Animal</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo URL.'contato'; ?>">Contato</a>
+        <a class="nav-link"  href="<?php echo URL.'sobre'; ?>">sobre</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <a href="<?php echo URL.'adm'; ?>" class="btn btn-secondary my-2 my-sm-0">Entrar</a>
+      <a href="<?php echo URL.'login'; ?>" class="btn btn-primary my-2 my-sm-0">Entrar</a>
     </form>
   </div>
 </nav>
