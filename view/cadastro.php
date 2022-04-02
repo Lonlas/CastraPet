271c2fc5c3518e59606ae5a750eb2b94cd65d778
Nicolas Longo <nicolaslongo18@gmail.com>
2022-04-02 19:07:35 -0300

    Criação do formulário HTML sem CSS 02.04.22

--- view/cadastro.php
+++ view/cadastro.php
@@ -11,7 +11,112 @@
 </head>
 <body>
     <!-- CORPO -->
-
+    <form method="post" action="#">
+        <h1>CADASTRAR</h1>
+        <table>
+            <tbody>
+                <tr colspan="2">
+                    <td>
+                        <label for="txtNome">Nome:</label>
+                        <input type="text" name="txtNome" id="txtNome" maxlength="70" required>
+                    </td>
+                </tr>
+                <tr colspan="2">
+                    <td>
+                        <label for="txtEmail">E-mail:</label>
+                        <input type="email" name="txtEmail" id="txtEmail" maxlength="100" required>
+                    </td>
+                </tr> 
+                <tr>
+                    <td>
+                        <label for="txtCPF">CPF:</label>
+                        <input type="text" name="txtCPF" id="txtCPF" maxlength="14" required>
+                    </td>
+                    <td>
+                        <label for="txtTel">Telefone:</label>
+                        <input type="text" name="txtTel" id="txtTel" maxlength="15">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="txtRG">RG:</label>
+                        <input type="text" name="txtRG" id="txtRG" maxlength="12" required>
+                    </td>
+                    <td>
+                        <label for="txtCelular">Celular:</label>
+                        <input type="text" name="txtCelular" id="txtCelular" maxlength="15" required>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <th><h2>Endereço</h2></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <label for="txtCEP">CEP:</label>
+                        <input type="text" name="txtCEP" id="txtCEP" maxlength="9" required> 
+                    </td>
+                    <td>
+                        <label for="txtNumero">Número</label>
+                        <input type="text" name="txtNumero" id="txtNumero" maxlength="5" required>
+                    </td>
+                </tr>
+                <tr colspan="2">
+                    <td>
+                        <label for="txtBairro">Bairro:</label>
+                        <input type="text" name="txtBairro" id="txtBairro" maxlength="50" required>
+                    </td>
+                </tr>
+                <tr colspan="2">
+                    <td>
+                        <label for="txtRua">Rua:</label>
+                        <input type="text" name="txtRua" id="txtRua" maxlength="50" required>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <label for="txtSenha">Crie uma senha:</label>
+                        <input type="password" name="txtSenha" id="txtSenha" maxlength="40" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="txtConfirmaSenha">Confirme sua senha:</label>
+                        <input type="password" name="txtConfirmaSenha" id="txtConfirmaSenha" maxlength="40" required>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <input type="checkbox" name="chkProtetor" id="chkProtetor">
+                        <label for="chkProtetor">Sou protetor de animais</label>
+                        &nbsp;
+                        <input type="button" value="Fazer upload" name="btnProtetorUpload">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="chkNIS" id="chkNIS">
+                        <label for="chkNIS">Tenho o benefício do NIS</label>
+                        <input type="text" name="txtNIS" id="txtNIS" disabled>
+                    </td>
+                    <td>
+                        <input type="submit" value="Cadastrar">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
     <!-- /CORPO -->
 
     <!-- EXTENSÃO BOOTSTRAP -->
