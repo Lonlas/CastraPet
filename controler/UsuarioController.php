1c761b192debde91610afe9719d42351973b24e3
LeugiMH <miguelhenriquegs369@gmail.com>
2022-04-02 23:34:06 -0300

    Início das funções do CRUD do usuário e animal

--- controler/UsuarioController.php
+++ controler/UsuarioController.php
@@ -11,8 +11,22 @@ class UsuarioController
         include "view/login.php";
     }
     function cadastrarUsuario(){
+        //Incompleto
         $cadastra = new Usuario();
-        $cadastra->idusuario = $_POST["nome"];
+        $cadastra->idlogin = $_POST["#"];
+        $cadastra->rg = $_POST["txtRG"];
+        $cadastra->cpf = $_POST["txtCPF"];
+        $cadastra->beneficio = $_POST["txtRG"];
+        $cadastra->telefone = $_POST["txtTel"];
+        $cadastra->celular = $_POST["txtCel"];
+        $cadastra->punicao = $_POST["txtCEP"];
+        $cadastra->usuarua = $_POST["txtRua"];
+        $cadastra->usubairro = $_POST["txtBairro"];
+        $cadastra->usunumero = $_POST["txtNumero"];
+        $cadastra->usucep = $_POST["txtCEP"];
+
+
+        $cadastra->cadastrar();
     }
 }
 ?>
