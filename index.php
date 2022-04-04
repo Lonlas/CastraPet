776a30ed98239c9aa83dd20a6c19523f1098027a
Nicolas Longo <nicolaslongo18@gmail.com>
2022-04-04 14:59:31 -0300

    Comentando index e corrigindo algumas coisas

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php
 
+//Importando arquivos da pasta Controller
 include "controller/Controller.php";
 include "controller/UsuarioController.php";
 include "controller/AnimalController.php";
+include "controller/RacaController.php";
 
+//Definindo uma constante para a URL do site
 define("URL","http://localhost/CastraPet/");
 if($_GET)
 {
+    //Pegando a URL e apagando a "/" no final dela.
     $url = $_GET["url"];
     $url = explode("/",$url);
 
+    //Definindo os nomes das telas que vão aparecer na URL
     switch($url[0])
     {   
-        case "inicio":
-           $direciona = new Controller();
-           $direciona->abrirInicio();
-        break;
-        case "cadastro-usuario";
+        case "cadastro-tutor";
             $direciona = new UsuarioController();
             $direciona->abrirCadastro();
         break;
-        case "cadastrar-usuario";
+        case "cadastrar-tutor";
             $direciona = new UsuarioController();
             $direciona->cadastrarUsuario();
         break;
@@ -28,7 +29,7 @@ if($_GET)
             $direciona = new UsuarioController();
             $direciona->abrirLogin();
         break;
-        case "esqueciasenha":
+        case "esqueci-a-senha":
             $direciona = new Controller();
             $direciona->abrirEsqSenha();
         break;
@@ -40,24 +41,38 @@ if($_GET)
             $direciona = new AnimalController();
             $direciona->abrirMeusAnimais();
             break;
-        case "cadastraanimal":
+        case "cadastro-animal":
             $direciona = new AnimalController();
             $direciona->abrirCadAnimal();
             break;
-        case "infoanimal":
+        case "cadastrar-animal":
+            $Animal = new AnimalController();
+            $Animal->cadastrarAnimal();
+            break;
+        case "info-animal":
             $direciona = new AnimalController();
             $direciona->abrirInfoAnimal();
+            break;
+        case "cadastro-raca":
+            $raca = new RacaController();
+            $raca->abrirCadRaca();
+            break;
+        case "cadastrar-raca":
+            $raca = new RacaController();
+            $raca->cadastrarRaca();
             break;
         case "teste":
             $direciona = new UsuarioController();
             $direciona->teste();
             break;
         default:
+            //Mostrando um aviso de erro para caso entre em uma URL inválida
             echo "Página não encontrada";
     }
 }
 else
 {
+    //Abrindo página inicial do site 
     $direciona = new Controller();
     $direciona->abrirInicio();
 }
