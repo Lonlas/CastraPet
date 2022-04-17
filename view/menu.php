19cb334b80ca757143ee68bfa9de3996563d45d5
MylenaKelly <mylenakelly555@gmail.com>
2022-04-17 01:58:07 -0300

    Mudei a cor do botão

--- view/menu.php
+++ view/menu.php
@@ -20,7 +20,7 @@
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
-      <a href="<?php echo URL.'login'; ?>" class="btn btn-primary my-2 my-sm-0">Entrar</a>
+      <a href="<?php echo URL.'login'; ?>" class="btn btn-success my-2 my-sm-0">Entrar</a>
     </form>
   </div>
 </nav>
