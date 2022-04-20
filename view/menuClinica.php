5aa7f7f91c508029094f9fa00ec0123bf23bc9d5
MylenaKelly <mylenakelly555@gmail.com>
2022-04-20 16:13:44 -0300

    Criei "menuClinica.php"

---
+++ view/menuClinica.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-md navbar-light bg-transparent" style="padding: 2rem 2rem;">
+  <a class="navbar-brand" href="<?php echo URL.'inicio'; ?>">Início</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link"  href="<?php echo URL; ?>">Inicío <span class="sr-only">(página atual)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link"  href="<?php echo URL.'#'; ?>">Consultar castrações</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <a href="<?php echo URL.'#'; ?>" class="btn btn-success my-2 my-sm-0">Login</a>
+    </form>
+  </div>
+</nav>
