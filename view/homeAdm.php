e6e42a5bb04f71cee88f21756072b18ab816af63
MylenaKelly <mylenakelly555@gmail.com>
2022-04-20 16:10:45 -0300

    Criei "homeAdm.php"

---
+++ view/homeAdm.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CastraPet</title>
+    <!-- EXTENSÃO BOOTSTRAP -->
+    <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/root.css">
+</head>
+<body>
+    <!-- CORPO -->
+    <?php include_once "menuADM.php";?>
+
+    <div class="container-fluid">
+        <div class="container-fluid bg-danger">
+            <div class="row p-3">
+                <div class="col-md-12 p-3 bg-white">
+                    
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus mi, venenatis et scelerisque a, consectetur eu neque. Cras id rhoncus tortor, eget     aliquam erat. Aliquam erat volutpat. Pellentesque vehicula quam quis elementum maximus. Suspendisse ut congue nibh. Quisque vitae imperdiet nulla, eget pellentesque orci. In nunc mauris, tristique ut ex nec, volutpat pretium elit. Etiam sollicitudin ullamcorper elit in ultricies.
+
+                    Donec cursus placerat commodo. Duis luctus arcu enim. Etiam lobortis fermentum aliquet. In lectus velit, aliquet tempor rhoncus et, efficitur sit amet nibh. Nulla quis odio id nulla rutrum pretium. Maecenas consequat consequat posuere. Nullam sed mattis sapien.
+
+                    Suspendisse vitae magna sed neque vestibulum fermentum. Quisque a vulputate sapien. Pellentesque condimentum scelerisque feugiat. Pellentesque luctus quam orci, ut aliquam lorem iaculis sed. Morbi ac metus ac nisl fermentum volutpat eu in nulla. Curabitur congue quam et urna porttitor condimentum. Aliquam lorem erat, euismod vel velit a, imperdiet luctus magna. Suspendisse aliquam sapien non nisi eleifend, et suscipit ante auctor. Vivamus rhoncus enim nulla, in dignissim sem elementum ut. Phasellus feugiat volutpat tempor. Sed tristique enim nec elit ornare varius. Mauris congue mauris vitae dui elementum vulputate. Suspendisse consequat, tellus nec tincidunt pellentesque, nisl augue fringilla odio, eget egestas nunc dui vel eros.
+
+                    Donec pharetra lacinia posuere. Integer lobortis dui vel suscipit ullamcorper. Morbi eget cursus ipsum, commodo mollis magna. Aliquam eu suscipit mi. Phasellus eu scelerisque est. Phasellus ac convallis leo. Integer maximus suscipit laoreet. Suspendisse non justo posuere nisi hendrerit efficitur. Nullam tortor justo, porttitor nec neque vel, lacinia commodo ipsum.
+
+                    Phasellus dictum dolor sit amet ultrices blandit. Phasellus iaculis tempor pharetra. Pellentesque facilisis eget tellus vel interdum. Morbi tincidunt mi eget mi finibus commodo. Maecenas sit amet rutrum lorem. Sed in sem vel ante molestie luctus. Vivamus aliquam turpis id ante dignissim, et congue sapien aliquam. Mauris quis varius mi, id varius velit. Maecenas consequat non diam vel scelerisque. Suspendisse potenti. Fusce purus massa, porta eget odio eu, hendrerit semper nisi. In risus quam, molestie at porta nec, aliquet eget felis. Duis ultricies venenatis justo id sollicitudin. Mauris non convallis lorem.
+                    
+                </div>
+            </div>
+        </div>
+        <footer class="container-fluid text-center" style="padding: 2.5rem; color:white; background:var(--preto);">
+        
+        </footer>
+    </div>
+    <!-- /CORPO -->
+
+    <!-- EXTENSÃO BOOTSTRAP -->
+    <script src="<?php echo URL; ?>recursos/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?php echo URL; ?>recursos/js/popper.min.js"></script>
+    <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>
+</body>
+</html>
