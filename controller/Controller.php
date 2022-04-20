9f3e1bdb002268feed5de42fc9a1ae316b4e33eb
MylenaKelly <mylenakelly555@gmail.com>
2022-04-20 16:05:54 -0300

    Criação de função teste

--- controller/Controller.php
+++ controller/Controller.php
@@ -10,5 +10,10 @@ class Controller
     function abrirPerfil(){
         include "view/infoUsuario.php";
     }
+    function abrirADM(){
+        //Função criada para visualizar as diferentes telas de níveis de acesso
+        //include "view/homeClinica.php";
+        include "view/homeAdm.php";
+    }
 }
 ?>
