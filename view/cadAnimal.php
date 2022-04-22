be86c0540020378195e3e3c4eb565adc5b0d7648
LeugiMH <miguelhenriquegs369@gmail.com>
2022-04-21 23:15:42 -0300

    Arrumando os Dropdowns da responsividade

--- view/cadAnimal.php
+++ view/cadAnimal.php
@@ -145,8 +145,8 @@
 
     <!-- EXTENSÃO BOOTSTRAP -->
     <script src="<?php echo URL; ?>recursos/js/jquery-3.3.1.slim.min.js"></script>
-    <!--<script src="<?php echo URL; ?>recursos/js/popper.min.js"></script> Ultrapassado
-    <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>-->
+    <!--<script src="<?php echo URL; ?>recursos/js/popper.min.js"></script> Ultrapassado-->
+    <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>
     <script src="<?php echo URL;?>recursos/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
