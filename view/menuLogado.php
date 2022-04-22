d6bf6b78b0ef17764cfbc68cecb6a89432f69d5d
MylenaKelly <mylenakelly555@gmail.com>
2022-04-22 01:30:41 -0300

    Mudei a URL de inicio e inseri o ícone do perfil

--- view/menuLogado.php
+++ view/menuLogado.php
@@ -6,7 +6,7 @@
 <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
     <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-        <a class="nav-link"  href="<?php echo URL; ?>">Inicío <span class="sr-only">(página atual)</span></a>
+        <a class="nav-link"  href="<?php echo URL.'home-usuario'; ?>">Inicío <span class="sr-only">(página atual)</span></a>
     </li>
     <li class="nav-item active">
         <a class="nav-link"  href="<?php echo URL.'cadastra-animal'; ?>">Cadastrar Animal</a>
@@ -18,10 +18,10 @@
         <a class="nav-link"  href="<?php echo URL.'#'; ?>">Prefeitura Franco da Rocha</a>
     </li>
     </ul>
-    <form class="form-inline ms-auto my-2 my-lg-0">
-    <span class="material-symbols-outlined align-middle fs-1 me-3">
-        account_circle
-    </span>
+    <form class="form-inline ms-auto my-2 my-lg-0"> 
+        <a class="pe-auto text-decoration-none" href="<?php echo URL.'perfil'?>">
+            <img src="recursos/img/Logo-Castra-Pet.svg" alt="Perfil" width="50" class="me-3">
+        </a>
         <a href="<?php echo URL.'login';?>" class="btn btn-success my-2 my-sm-0">Encerrar Sessão</a>
     </form>
 </div>
