996623134f04b703f0f997cab8df09abe8f474c8
Etec <etec@etec.com>
2022-04-25 15:35:20 -0300

    Cria do a consulta usuário

---
+++ view/consultaUsuario.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CastraPet</title>
+    <!-- EXTENSÃO BOOTSTRAP -->
+    <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/root.css">
+</head>
+<body>
+    <!-- CORPO -->
+    <?php include_once "menuADM.php";?>
+
+    <div class="container-fluid">
+        <div class="bg-danger">
+            <div class="container mx-auto row p-3">
+                <form action="">    
+                    <div class="container bg-dark text-light font-weight-bold p-3">
+                        <label>Consultar Usuário:</label>
+                    </div>
+                </form>
+                <div class="container bg-white p-0">
+                    
+                    
+                </div>
+            </div>
+        </div>
+        <footer class="container-fluid text-left bg-dark" style="padding: 2.5rem; color:white; background:var(--preto);">
+            <a href="<?php echo URL.'home-adm'; ?>" class="btn btn-success my-2 my-sm-0">Voltar</a>
+        </footer>
+    </div>
+    <!-- /CORPO -->
+
+    <!-- EXTENSÃO BOOTSTRAP -->
+    <script src="<?php echo URL; ?>recursos/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="<?php echo URL; ?>recursos/js/popper.min.js"></script>
+    <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>
+</body>
+</html>
