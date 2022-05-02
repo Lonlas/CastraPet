3ad9a97b7a0559003f7a7624fc080e92628ed467
MylenaKelly <mylenakelly555@gmail.com>
2022-05-01 22:42:41 -0300

    Arrumei a tela de cadastro

--- view/cadastro.php
+++ view/cadastro.php
@@ -22,141 +22,87 @@
                 </div>
                 <div class="container bg-white">
                     <form method="post" action="cadastrar-tutor">
-                    <div class="row">
-                        <div class="col-6 float-md-end mb-3 ms-md-3">
-                            <table class="table table-borderless">
-                                <tbody>
-                                    <tr>
-                                        <td class="row-mb-1">
-                                            <label for="txtNome" class="col-form-label">Nome:</label>
-                                        </td>
-                                        <td colspan="3" class="row-mb-5">
-                                            <input  class="form-control" type="text" name="txtNome" id="txtNome" maxlength="70" required>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="row-mb-2">
-                                            <label for="txtEmail">Email:</label>
-                                        </td> 
-                                        <td colspan="3" class="row-mb-5"> 
-                                            <input class="form-control" type="email" name="txtEmail" id="txtEmail" maxlength="100" required>
-                                        </td>
-                                    </tr> 
-                                    <tr>
-                                        <td class="row-mb-1">
-                                            <label for="txtCPF">CPF:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtCPF" id="txtCPF" maxlength="14" required>
-                                        </td>
-                                        <td class="row-mb-1">
-                                            <label for="txtTel">Telefone:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtTel" id="txtTel" maxlength="15">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="row-mb-1">
-                                            <label for="txtRG">RG:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtRG" id="txtRG" maxlength="12" required>
-                                        </td>
-                                        <td class="row-mb-1">
-                                            <label for="txtCelular">Celular:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtCelular" id="txtCelular" maxlength="15">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" class="row-mb-3">
-                                            <input type="checkbox" name="chkProtetor" id="chkProtetor" value="1">
-                                            <label for="chkProtetor">Sou protetor de animais</label>
-                                        </td>
-                                        <td colspan="2" class="row-mb-3">
-                                            &nbsp;
-                                            <input class="btn btn-success" type="button" value="Fazer upload" name="btnProtetorUpload">
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" class="row-mb-3">
-                                            <input type="checkbox" name="chkNIS" id="chkNIS" value="2">
-                                            <label for="chkNIS">Tenho o benefício do NIS</label>
-                                        </td>
-                                        <td colspan="2" class="row-mb-3">
-                                            <input class="form-control" type="text" name="txtNIS" id="txtNIS">
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                    <div class="row m-0 align-items-center">
+                        <div class="col-md-6 mb-4 align-self-center">
+                            <div class="form-group row-6 mb-3">
+                                <label for="txtNome" class="col-form-label">Nome:</label>
+                                <input  class="form-control" type="text" name="txtNome" id="txtNome" maxlength="70" required>
+                            </div>
+                            <div class="form-group row-6 mb-3">
+                                <label for="txtEmail">Email:</label>
+                                <input class="form-control" type="email" name="txtEmail" id="txtEmail" maxlength="100" required>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="form-group col-6">
+                                    <label for="txtCPF">CPF:</label>
+                                    <input class="form-control" type="text" name="txtCPF" id="txtCPF" maxlength="14" required>
+                                </div>
+                                <div class="form-group col-6">
+                                    <label for="txtTel">Telefone:</label>
+                                    <input class="form-control" type="text" name="txtTel" id="txtTel" maxlength="15">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="form-group col-6">
+                                    <label for="txtRG">RG:</label>
+                                    <input class="form-control" type="text" name="txtRG" id="txtRG" maxlength="12" required>
+                                </div>
+                                <div class="form-group col-6">
+                                    <label for="txtCelular">Celular:</label>
+                                    <input class="form-control" type="text" name="txtCelular" id="txtCelular" maxlength="15">
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <div class="form-group col-6">
+                                    <input type="checkbox" name="chkNIS" id="chkNIS" value="2">
+                                    <label for="chkNIS">Tenho o benefício do NIS</label>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <input class="form-control" type="text" name="txtNIS" id="txtNIS">
+                                </div>
+                            </div>
+                            <div class="form-group mb-4">
+                                <input type="checkbox" name="chkProtetor" id="chkProtetor" value="1">
+                                <label for="chkProtetor">Sou protetor de animais</label>
+                                &nbsp;
+                                <input class="btn btn-success" type="button" value="Fazer upload" name="btnProtetorUpload">
+                            </div>
                         </div>
-                        <div class="col-6 float-md-end mb-3 ms-md-3">
-                            <table class="table table-borderless">
-                                <thead>
-                                    <tr>
-                                        <th colspan="4"><h5>Endereço</h5></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="row-mb-1">
-                                            <label for="txtCEP">CEP:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtCEP" id="txtCEP" maxlength="9" required> 
-                                        </td>
-                                        <td class="row-mb-1">
-                                            <label for="txtNumero">Número:</label>
-                                        </td>
-                                        <td class="row-mb-2">
-                                            <input class="form-control" type="text" name="txtNumero" id="txtNumero" maxlength="5" required>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="row-mb-2">
-                                            <label for="txtBairro">Bairro:</label>
-                                        </td>
-                                        <td colspan="3" class="row-mb-4">
-                                            <input class="form-control" type="text" name="txtBairro" id="txtBairro" maxlength="50" required>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="row-mb-2" >
-                                            <label for="txtRua">Rua:</label>
-                                        </td>
-                                        <td colspan="3" class="row-mb-4">
-                                            <input class="form-control" type="text" name="txtRua" id="txtRua" maxlength="50" required>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <td colspan="2" class="row-mb-2">
-                                            <label for="txtSenha">Crie uma senha:</label>
-                                        </td>
-                                        <td colspan="3" class="row-mb-4">
-                                            <input class="form-control" type="password" name="txtSenha" id="txtSenha" maxlength="40" required>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="2" class="row-mb-2">
-                                            <label for="txtConfirmaSenha">Confirme sua senha:</label>
-                                        </td>
-                                        <td colspan="2" class="row-mb-4">
-                                            <input class="form-control" type="password" name="txtConfirmaSenha" id="txtConfirmaSenha" maxlength="40" required>
-                                        </td>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
+                        <div class="col-md-6 mb-4 align-self-center justify-content-evenly">
+                            <div class="row mb-3">
+                                <div class="form-group col-6">
+                                    <label for="txtCEP">CEP:</label>
+                                    <input class="form-control" type="text" name="txtCEP" id="txtCEP" maxlength="9" required> 
+                                </div>
+                                <div class="form-group col-6">
+                                    <label for="txtNumero">Número:</label>
+                                    <input class="form-control" type="text" name="txtNumero" id="txtNumero" maxlength="5" required>
+                                </div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="txtBairro">Bairro:</label>
+                                <input class="form-control" type="text" name="txtBairro" id="txtBairro" maxlength="50" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="txtRua">Rua:</label>
+                                <input class="form-control" type="text" name="txtRua" id="txtRua" maxlength="50" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="txtSenha">Crie uma senha:</label>
+                                <input class="form-control" type="password" name="txtSenha" id="txtSenha" maxlength="40" required>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="txtConfirmaSenha">Confirme sua senha:</label>
+                                <input class="form-control" type="password" name="txtConfirmaSenha" id="txtConfirmaSenha" maxlength="40" required>
+                            </div>
+                            <div class="row mb-1">
+                                <div class="col-9"></div>
+                                <div class=" form-group col-3">
+                                    <input type="submit" class="btn btn-success" value="Cadastrar">
+                                </div>        
+                            </div>
+                        </div>     
                     </div>
-                    <div class="row">
-                        <div class="col-md-12 mb-4 ms-md-3 text-right">
-                            <input type="submit" class="btn btn-success justify-content-md-end col-2 mx-auto" value="Cadastrar">
-                        </div>       
-                    </div>  
                     </form>
                 </div>
             </div>
