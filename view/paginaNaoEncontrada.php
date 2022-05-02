8471c90e550e819520d38cdcafdd41732f1a7bb8
LeugiMH <miguelhenriquegs369@gmail.com>
2022-05-02 16:39:31 -0300

    Adição dos Favicons

--- view/paginaNaoEncontrada.php
+++ view/paginaNaoEncontrada.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <?php include_once "favicon.php"?>
     <title>CastraPet</title>
     <!-- EXTENSÃO BOOTSTRAP -->
     <link rel="stylesheet" href="<?php echo URL;?>recursos/css/bootstrap.min.css">
