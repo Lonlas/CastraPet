eadf71493deb8356bb01187703c23284fb8c3d6e
MylenaKelly <mylenakelly555@gmail.com>
2022-05-03 23:00:48 -0300

    Criei tela do perfil do usuário

--- view/infoUsuario.php
+++ view/infoUsuario.php
@@ -18,20 +18,78 @@
         <div class="container-fluid bg-primary">
             <div class="container mx-auto row p-3">
                 <div class="container bg-dark text-light font-weight-bold p-3">
-                    Olá usuário!
+                    Olá <?php echo" usuário !"; ?>
                 </div>
                 <div class="container bg-white ">
-                    <div class="row">
-                        <div class="col">
-                        <!-- Componentes aqui -->
-
+                    <form action="<?php echo URL.'perfil'; ?>">
+                        <div class="row align-items-center justify-content-center mb-2">
+                            <div class="col-sm-5 mb-3 form-group ps-4">
+                                <div class="row">
+                                    <p>Nome:<?php echo" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";?></p>
+                                </div>
+                                <div class="row">
+                                    <p>E-mail:<?php echo" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";?></p>
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <p>CPF:<?php echo" xxxxxxxxxxx";?></p>
+                                    </div>
+                                    <div class="col-6">
+                                        <p>Telefone:<?php echo" xxxxxxxxxxx";?></p>
+                                    </div>    
+                                </div>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <p>RG:<?php echo" xxxxxxxxxxx";?></p>
+                                    </div>
+                                    <div class="col-6">
+                                        <p>Celular:<?php echo" xxxxxxxxxxx";?></p>
+                                    </div>    
+                                </div>
+                                
+                            </div>
+                            <div class="col-sm-7 mb-2 form-group ps-4">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <p>CEP:<?php echo" xxxxxxxxx";?></p>
+                                    </div>
+                                    <div class="col-6">
+                                        <p>Número:<?php echo" xxxxx";?></p>
+                                    </div>    
+                                </div>
+                                <div class="row">
+                                    <p>Bairro:<?php echo" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";?></p>
+                                </div>
+                                <div class="row">
+                                    <p>Rua:<?php echo" xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";?></p>
+                                </div>
+                                <div class="row">
+                                    <p>
+                                        <input type="checkbox" name="chkProtetor" id="chkProtetor" checked disabled>
+                                        Tenho benefício NIS: <?php echo" xxxxxxxx"?>
+                                    </p>
+                                </div>
+                                
+                                <div class="row ">
+                                    <p>
+                                        <input type="checkbox" name="chkProtetor" id="chkProtetor" checked disabled>
+                                        Sou protetor de animais
+                                        &nbsp;
+                                        <input class="btn btn-success col-auto" type="button" value="Visualizar documento" name="btnProtetorDoc"> 
+                                    </p>
+                                </div>
+                                
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 mb-4 ms-md-3 text-right">
-                            <a href="<?php echo URL.'meus-animais'; ?>" class="btn btn-success justify-content-end col-2 mx-auto">Meus animais</a>
+                        <div class="row align-items-center justify-content-between ps-3  mb-4">
+                            <div class="col-sm-3 mb-3">
+                                <a href="<?php echo URL.'alterar-senha'; ?>" class="link-dark">Alterar minha senha</a>
+                            </div>
+                            <div class="col-sm-9">
+                                <a href="<?php echo URL.'meus-animais'; ?>" class="btn btn-success col-auto ">Meus animais</a>
+                            </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
