71503532de6612701f15daa956286d2825601a5e
Nicolas Longo <nicolaslongo18@gmail.com>
2022-05-09 17:49:09 -0300

    Tentando colocar DataTables

--- view/consultaCastracao.php
+++ view/consultaCastracao.php
@@ -7,66 +7,51 @@
     <title>CastraPet</title>
     <!-- EXTENSÃO BOOTSTRAP -->
     <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/bootstrap.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
 
 </head>
 <body>
     <!-- CORPO -->
+    <?php include_once "menuADM.php";?>
+
     <div class="container-fluid">
-        <div class="container bg-danger mx-auto">
-            <div class="container bg-dark">
-                <h1 class="h3">Consultar de Castrações : </h1><input type="text">
-            </div>
-            <div class="container bg-white">
-                <table class="table">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label for="">Número da castração:</label>
-                                <input type="text" name="" id="">
-                            </td>
-                            <td>
-                                <label for="">Data:</label>
-                                <input type="text" name="" id="">
-                            </td>
-                            <td>
-                                <label for="">Hora:</label>
-                                <input type="text" name="" id="">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="">Nome do animal:</label>
-                                <input type="text" name="" id="">
-                            </td>
-                            <td>
-                                <label for="">CPF do responsável:</label>
-                                <input type="text" name="" id="">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="">Nome da clínica</label>
-                                <input type="text" name="" id="">
-                            </td>
-                            <td>
-                                <label for="">CNPJ da clínica</label>
-                                <input type="text" name="" id="">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="">Observações:</label>
-                                <textarea name="" id="" cols="30" rows="10"></textarea>
-                            </td>
-                            <td>
-                                <label for="">Status:</label>
-                                <select name="" id="">
-                                    <option></option>
-                                </select>
-                            </td>
-                        </tr>
+        <div class="bg-danger">
+            <div class="container mx-auto p-3">
+                <div class="container bg-dark p-2">
+                    <h1 class="h4 text-white ms-3">Consultar Castrações</h1>
+                </div>
+                <div class="bg-white p-3">
+                    <table id="tbCastracao">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Animal</th>
+                                <th>Tutor</th>
+                                <th>Clínica</th>
+                                <th>Horário</th>
+                                <th>Status</th>
+                                <th>Observação</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            echo 
+                            "
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            ";
+                            ?>
                         </tbody>
-                </table>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -76,5 +61,23 @@
     <script src="<?php echo URL; ?>recursos/js/jquery-3.3.1.slim.min.js"></script>
     <script src="<?php echo URL; ?>recursos/js/popper.min.js"></script>
     <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>
+
+    <!-- DataTables -->
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#tbCastracao').DataTable( {
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf'
+                ]
+            } );
+        } );
+    </script>
 </body>
 </html>
