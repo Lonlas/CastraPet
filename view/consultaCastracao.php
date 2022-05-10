6e7a4cc1d4b0089096604d79aebedfe5b6c2172d
Nicolas Longo <nicolaslongo18@gmail.com>
2022-05-10 17:44:51 -0300

    Consultar castração pronto

--- view/consultaCastracao.php
+++ view/consultaCastracao.php
@@ -7,6 +7,7 @@
     <title>CastraPet</title>
     <!-- EXTENSÃO BOOTSTRAP -->
     <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/root.css">
     <!-- DataTables -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
 
@@ -22,36 +23,47 @@
                     <h1 class="h4 text-white ms-3">Consultar Castrações</h1>
                 </div>
                 <div class="bg-white p-3">
-                    <table id="tbCastracao">
+                    <table id="tbCastracao" class="table table-">
                         <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Animal</th>
-                                <th>Tutor</th>
+                                <th>CPF do Responsável</th>
                                 <th>Clínica</th>
-                                <th>Horário</th>
+                                <th>Data</th>
+                                <th>Hora</th>
                                 <th>Status</th>
                                 <th>Observação</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
-                            echo 
-                            "
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            ";
+                            foreach($dadosCastracao as $value)
+                            {
+                                echo 
+                                "
+                                <tr>
+                                    <td>$value->idcastracao</td>
+                                    <td>$value->aninome</td>
+                                    <td>$value->cpf</td>
+                                    <td>$value->nomeclinica</td>
+                                    <td>". date('d/m/Y',strtotime($value->horario)) ."</td>
+                                    <td>". date('H:i',strtotime($value->horario)) ."</td>
+                                    <td>$value->status</td>
+                                    <td>$value->observacao</td>
+                                </tr>
+                                ";
+                            }
                             ?>
                         </tbody>
                     </table>
                 </div>
+            </div>
+            <div class="col" style="background:var(--preto); padding: 35px 0px 35px 0px; overflow: hidden;">
+                <a href="#" class="btn-lg btn-success" role="button" style="border-radius: 0; text-decoration: 0; padding: 12px 35px 12px 35px; margin-left: 40px;">Voltar</a>
+            </div>
+            <div class="container">
+                
             </div>
         </div>
     </div>
@@ -65,19 +77,27 @@
     <!-- DataTables -->
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
     <script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+
     <script>
         $(document).ready(function() {
             $('#tbCastracao').DataTable( {
                 dom: 'Bfrtip',
                 buttons: [
-                    'copy', 'csv', 'excel', 'pdf'
-                ]
+                    'csv', 'excel', 'print'
+                ],
+                "language": {
+                    "url": "//cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json"
+                }
             } );
         } );
-    </script>
+        </script>
+    <!--<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json"></script>-->
+
 </body>
 </html>
