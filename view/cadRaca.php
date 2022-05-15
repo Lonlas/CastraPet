5d38fb91748d89750324ecb6cb2637880406bf30
LeugiMH <miguelhenriquegs369@gmail.com>
2022-05-15 17:53:52 -0300

    Ajuste do tamanho da tela cadRaça

--- view/cadRaca.php
+++ view/cadRaca.php
@@ -10,12 +10,17 @@
     <!-- EXTENSÃO BOOTSTRAP -->
     <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo URL; ?>recursos/css/root.css">
-
+    <style rel="stylesheet" type="text/css">
+        .corpo{
+            grid-template-areas: 'header''corpo''footer';
+            grid-template-rows:  100px auto 100px;
+        }
+    </style>
 </head>
 <body>
 
     <!-- CORPO -->
-    <div class="container-fluid" style="height:100vh;">
+    <div class="container-fluid d-grid min-vh-100 corpo">
         <?php //CONTROLE DE MENU
             if($_SESSION) //caso esteja logado e exista uma sessão
                 {
@@ -39,33 +44,39 @@
                 include_once "menu.php";
             }
         ?>
-        <div class="bg-danger" style="height:auto;">
-            <div class="container mx-auto row align-middle p-3">
-                <div class="container bg-dark text-light font-weight-bold p-3">
-                    Cadastro de Raça
-                </div>
-                <div class="container bg-white">
-                    <form action="cadastrar-raca" class="p-sm-2 py-sm-3 p-md-3 p-lg-4 py-3 " method="POST">
-                        <label for="Especie">Espécie:</label>
-                        <select name="tipoEspecie" id="Especie" class="form-select">
-                            <option value="1">Felina</option>
-                            <option value="0">Canina</option>
-                        </select>
-                        <small class="form-text text-muted">Selecione a Espécie dessa Raça</small>
-                        <div class="form-group">
-                            <label for="raca">Raça:</label>
-                            <input type="text" id="raca" name="txtRaca" class="form-control" placeholder="ex: Siamês, Persa, Sphynx " required>
-                            <small class="form-text text-muted">Informe o nome da raça que deseja cadastrar</small>
-                        </div>
-                        <div class="text-center">
-                            <input type="submit" class="btn btn-success align-middle" value="Cadastrar">
-                        </div>
-                    </form>
+        <div class="bg-danger container-fluid" style="grid-area: corpo;">
+            <div class="row h-100 align-items-center">
+                <div>
+                    <div class="container bg-dark text-light font-weight-bold p-3">
+                        Cadastro de Raça
+                    </div>
+                    <div class="container bg-white">
+                        <form action="cadastrar-raca" class="p-sm-2 py-sm-3 p-md-3 p-lg-4 py-3" method="POST">
+                            <label for="Especie">Espécie:</label>
+                            <select name="tipoEspecie" id="Especie" class="form-select">
+                                <option value="1">Felina</option>
+                                <option value="0">Canina</option>
+                            </select>
+                            <small class="form-text text-muted">Selecione a Espécie dessa Raça</small>
+                            <div class="form-group">
+                                <label for="raca">Raça:</label>
+                                <input type="text" id="raca" name="txtRaca" class="form-control" placeholder="ex: Siamês, Persa, Sphynx " required>
+                                <small class="form-text text-muted">Informe o nome da raça que deseja cadastrar</small>
+                            </div>
+                            <div class="text-center">
+                                <input type="submit" class="btn btn-success align-middle" value="Cadastrar">
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="container-fluid bg-dark align-text-bottom" style="padding: 2.5rem; color:white; background:var(--preto);">
-            <a href="<?php echo URL.'home-adm'; ?>" class="btn btn-success my-2 my-sm-0">Voltar</a>
+        <div class="container-fluid bg-dark" style="grid-area: footer;">
+            <div class="row h-100 align-items-center">
+                <div class="px-5">
+                    <a href="<?php echo URL.'home-adm';?>" class="btn btn-success">Voltar</a>
+                </div>
+            </div> 
         </div>
     </div>
     
