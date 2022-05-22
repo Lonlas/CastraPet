2c114b258047d7c9d754d3810704dbd786fcf40e
LeugiMH <miguelhenriquegs369@gmail.com>
2022-05-22 11:16:41 -0300

    Criação da head

---
+++ view/head.php
@@ -0,0 +1,13 @@
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicon -->
+    <?php include_once "favicon.php"?>
+    <!-- /Favicon-->
+
+    <title>CastraPet</title>
+    <!-- EXTENSÃO BOOTSTRAP -->
+    <link rel="stylesheet" href="<?php echo URL;?>recursos/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo URL;?>recursos/css/root.css">
+    <!-- /EXTENSÃO BOOTSTRAP -->
