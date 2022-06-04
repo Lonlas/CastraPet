5d3eac7710c3867fc15e13612f9cb301d485d161
LeugiMH <miguelhenriquegs369@gmail.com>
2022-06-04 18:04:23 -0300

    confirmar a senha funcional cadUsu e altSenhaUsu

--- view/alterarSenha.php
+++ view/alterarSenha.php
@@ -5,52 +5,59 @@
 </head>
 <body>
     <!-- CORPO -->
-    <?php //CONTROLE DE MENU
-        if($_SESSION) //caso esteja logado e exista uma sessão
-        {
-            switch($_SESSION["dadosLogin"]->nivelacesso)
+    <div class="container-fluid d-grid min-vh-100 corpo">
+        <?php //CONTROLE DE MENU
+            if($_SESSION) //caso esteja logado e exista uma sessão
             {
-                //caso tenha nível de acesso de usuário
-                case 0: include_once "menuLogado.php"; break;
-                //caso tenha nível de acesso de clínica
-                case 1: include_once "menuClinica.php"; break;
-                //caso tenha nível de acesso de Administrador
-                case 2: include_once "menuADM.php"; break;   
+                switch($_SESSION["dadosLogin"]->nivelacesso)
+                {
+                    //caso tenha nível de acesso de usuário
+                    case 0: include_once "menuLogado.php"; break;
+                    //caso tenha nível de acesso de clínica
+                    case 1: include_once "menuClinica.php"; break;
+                    //caso tenha nível de acesso de Administrador
+                    case 2: include_once "menuADM.php"; break;   
+                }
             }
-        }
-        else{ include_once "menu.php"; }
-    ?>
-
-    <div class="container-fluid">
-        <div class="container-fluid bg-primary">
-            <div class="container mx-auto row p-3">
-                <div class="container bg-dark text-light font-weight-bold p-3">
-                    Alterar minha senha
-                </div>
-                <div class="container bg-white">
-                    <!-- Componentes aqui -->
-                    <form action="<?php echo URL.'perfil'; ?>">
-                        <div class="row align-items-center justify-content-center my-4 ">
-                            <div class="col-md-6">
-                                <label class="form-label">Insira uma nova senha:</label>
-                                <input type="password" name="novaSenha" id="novaSenha" class="form-control">
-
-                                <label class="form-label">Confirme sua senha:</label>
-                                <input type="password" name="novaSenha" id="novaSenha" class="form-control">
+            else{ include_once "menu.php"; }
+        ?>
+        <div class="bg-primary container-fluid" style="grid-area: corpo;">
+            <div class="row h-100 align-items-center">
+                <div class="p-3">
+                    <div class="container bg-dark text-light font-weight-bold p-3">
+                        <h5 class="m-0">Alterar minha senha</h5>
+                    </div>
+                    <div class="container p-sm-3 p-md-3 p-lg-4 p-3 px-0 bg-white">
+                        <form action="<?php echo URL.'perfil'; ?>" class="p-sm-3 p-md-3 p-lg-4 p-3 px-0 m-0" method="post">
+                            <div class="row align-items-center justify-content-center m-0">
+                                <div class="form-group">
+                                    <label class="form-label" for="novaSenha">Insira uma nova senha:</label>
+                                    <input type="password" name="novaSenha" id="novaSenha" required  class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-label" for="novaSenha">Confirme sua senha:</label>
+                                    <input type="password" name="confSenha" id="confSenha" required  class="form-control">
+                                </div>
+                                <div class="text-danger" id="avisoIgualdade" style="display:none;">as Senhas devem ser iguais*</div>
+                                <div class="text-danger" id="avisoComprimento" style="display:none;">A senha deve conter mais que 5 dígitos*</div>
+                                <div>
+                                    <input type="submit" value="Alterar" class="btn btn-success col-auto mt-3 float-end" name="alterarSenha">
+                                </div>
                             </div>
-                        </div>
-                        <div class="row align-items-center justify-content-end my-3 me-3">
-                            <input type="submit" value="Alterar" class="btn btn-success col-auto" name="alterarSenha">
-                        </div>
-                    </form>              
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <footer class="container-fluid text-left bg-dark" style="padding: 2.5rem; color:white; background:var(--preto);">
-            <!--<a href="<?php echo URL.'meus-animais'; ?>" class="btn btn-success my-2 my-sm-0">Voltar</a>-->
-        </footer>
+        <div class="container-fluid bg-dark" style="grid-area: footer;">
+            <div class="row h-100 align-items-center">
+                <div class="px-5">
+                    <a href="<?php echo URL.'home-usuario';?>" class="btn btn-success">Voltar</a>
+                </div>
+            </div> 
+        </div>
     </div>
-
+    
     <!-- /CORPO -->
 
     <!-- EXTENSÃO BOOTSTRAP -->
@@ -58,5 +65,26 @@
     <!--<script src="<?php echo URL; ?>recursos/js/popper.min.js"></script> Ultrapassado -->
     <script src="<?php echo URL; ?>recursos/js/bootstrap.min.js"></script>
     <script src="<?php echo URL;?>recursos/js/bootstrap.bundle.min.js"></script>
+
+    <script type="text/javascript">
+        //confirme a senha
+        $("form").submit(function(){
+            if($("#novaSenha").val() != $("#confSenha").val())
+            {
+                event.preventDefault();
+                $("#avisoIgualdade").show();
+            }
+            else{$("#avisoIgualdade").hide();}
+
+            if(($("#novaSenha").val().length <= 5) && ($("#confSenha").val().length <= 5))
+            {
+                event.preventDefault();
+                $("#avisoComprimento").show();
+            }
+            else {$("#avisoComprimento").hide();}
+        });
+    </script>
+
+
 </body>
 </html>
