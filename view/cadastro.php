2c7de56cc7c10ce4ab85d2a5ee2e5d40841480ec
Nicolas Longo <nicolaslongo18@gmail.com>
2022-06-06 21:24:32 -0300

    Upload comprovante e melhorias

--- view/cadastro.php
+++ view/cadastro.php
@@ -38,31 +38,40 @@
                         <form method="post" class="p-sm-3 p-md-3 p-lg-4 p-3 px-0 row m-0" action="cadastrar-tutor" enctype="multipart/form-data">
                             <div class="col-md-6 mb-lg-0 mb-5 p-0 pe-md-3">
                                 <div class="form-group row-6 mb-3">
-                                    <label for="txtNome" class="form-label">Nome:</label>
+                                    <label for="txtNome" class="form-label">Nome:<font color="red">*</font></label>
                                     <input  class="form-control" type="text" name="txtNome" id="txtNome" maxlength="70" required>
                                 </div>
                                 <div class="form-group row-6 mb-3">
-                                    <label for="txtEmail" class="form-label">Email:</label>
+                                    <label for="txtEmail" class="form-label">Email:<font color="red">*</font></label>
                                     <input class="form-control" type="email" name="txtEmail" id="txtEmail" maxlength="100" required>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="form-group col-6">
-                                        <label for="txtCPF" class="form-label">CPF:</label>
-                                        <input class="form-control" type="text" name="txtCPF" id="txtCPF" maxlength="14" required>
+                                        <label for="txtCPF" class="form-label">CPF:<font color="red">*</font></label>
+                                        <input class="form-control" type="text" name="txtCPF" id="txtCPF" maxlength="14" minlength="11" placeholder="000.000.000-00" required>
                                     </div>
                                     <div class="form-group col-6">
                                         <label for="txtTel" class="form-label">Telefone:</label>
-                                        <input class="form-control" type="text" name="txtTel" id="txtTel" maxlength="15">
+                                        <input class="form-control" type="text" name="txtTel" id="txtTel" placeholder="(00) 0000 0000" maxlength="15">
                                     </div>
                                 </div>
                                 <div class="row mb-3">
                                     <div class="form-group col-6">
-                                        <label for="txtRG" class="form-label">RG:</label>
-                                        <input class="form-control" type="text" name="txtRG" id="txtRG" maxlength="12" required>
+                                        <label for="txtRG" class="form-label">RG:<font color="red">*</font></label>
+                                        <input class="form-control" type="text" name="txtRG" id="txtRG" maxlength="12" placeholder="00.000.000-0" required>
                                     </div>
                                     <div class="form-group col-6">
-                                        <label for="txtCelular" class="form-label">Celular:</label>
-                                        <input class="form-control" type="text" name="txtCelular" id="txtCelular" maxlength="15">
+                                        <label for="txtCelular" class="form-label">Celular:<font color="red">*</font></label>
+                                        <input class="form-control" type="text" name="txtCelular" id="txtCelular" placeholder="(00) 00000 0000" maxlength="15">
+                                    </div>
+                                </div>
+                                <div class="row mb-3">
+                                    <div class="form-group col-sm-6">
+                                        <label for="chkProtetor" class="form-label">Comprovante de residência:<font color="red">*</font></label>
+                                    </div>
+                                    <div class="form-group col-sm-6">
+                                        <input class="btn btn-success" type="file" value="Fazer upload do documento" accept="image/*" name="btnComprovante" id="btnComprovante" hidden>
+                                        <label id="labelComprovante" for="btnComprovante" class="btn btn-success" style="background-color: 0;">Enviar comprovante de residência</label>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -71,7 +80,7 @@
                                         <label for="chkNIS" class="form-label">Tenho o benefício do NIS</label>
                                     </div>
                                     <div class="form-group col-sm-6">
-                                        <input class="form-control" type="text" name="txtNIS" id="txtNIS" placeholder="Número do NIS" disabled>
+                                        <input class="form-control" type="text" name="txtNIS" id="txtNIS" placeholder="Número do NIS" maxlength="14" disabled>
                                     </div>
                                 </div>
                                 <div class="row mb-3">
@@ -80,36 +89,36 @@
                                         <label for="chkProtetor" class="form-label">Sou protetor de animais</label>
                                     </div>
                                     <div class="form-group col-sm-6">
-                                        <input class="btn btn-success" type="file" value="Fazer upload" accept="image/*" name="btnProtetorUpload" id="btnProtetorUpload" hidden>
-                                        <label id="labelProtetor" for="btnProtetorUpload" class="btn btn-success" style="background-color: 0;">Fazer upload</label>
+                                        <input class="btn btn-success" type="file" value="Fazer upload do documento" accept="image/*" name="btnProtetorUpload" id="btnProtetorUpload" hidden disabled>
+                                        <label id="labelProtetor" for="btnProtetorUpload" class="btn btn-success disabled" style="background-color: 0;">Fazer upload do documento</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-md-6 p-0 ps-md-3">
                                 <div class="row mb-3">
                                     <div class="form-group col-6">
-                                        <label for="txtCEP" class="form-label">CEP:</label>
-                                        <input class="form-control" type="text" name="txtCEP" id="txtCEP" maxlength="9" required> 
+                                        <label for="txtCEP" class="form-label">CEP:<font color="red">*</font></label>
+                                        <input class="form-control" type="text" name="txtCEP" id="txtCEP" maxlength="9" placeholder="00000-000" required> 
                                     </div>
                                     <div class="form-group col-6">
-                                        <label for="txtNumero" class="form-label">Número:</label>
+                                        <label for="txtNumero" class="form-label">Número:<font color="red">*</font></label>
                                         <input class="form-control" type="text" name="txtNumero" id="txtNumero" maxlength="5" required>
                                     </div>
                                 </div>
                                 <div class="form-group mb-3">
-                                    <label for="txtBairro" class="form-label">Bairro:</label>
+                                    <label for="txtBairro" class="form-label">Bairro:<font color="red">*</font></label>
                                     <input class="form-control" type="text" name="txtBairro" id="txtBairro" maxlength="50" required>
                                 </div>
                                 <div class="form-group mb-3">
-                                    <label for="txtRua" class="form-label">Rua:</label>
+                                    <label for="txtRua" class="form-label">Rua:<font color="red">*</font></label>
                                     <input class="form-control" type="text" name="txtRua" id="txtRua" maxlength="50" required>
                                 </div>
                                 <div class="form-group mb-3">
-                                    <label for="txtSenha" class="form-label">Crie uma senha:</label>
+                                    <label for="txtSenha" class="form-label">Crie uma senha:<font color="red">*</font></label>
                                     <input class="form-control" type="password" name="txtSenha" id="txtSenha" maxlength="40" required>
                                 </div>
                                 <div class="form-group mb-3">
-                                    <label for="txtConfirmaSenha" class="form-label">Confirme sua senha:</label>
+                                    <label for="txtConfirmaSenha" class="form-label">Confirme sua senha:<font color="red">*</font></label>
                                     <input class="form-control" type="password" name="txtConfirmaSenha" id="txtConfirmaSenha" maxlength="40" required>
                                     <div class="text-danger" id="avisoIgualdade" style="display:none;">as Senhas devem ser iguais*</div>
                                     <div class="text-danger" id="avisoComprimento" style="display:none;">A senha deve conter mais que 5 dígitos*</div>
@@ -152,6 +161,7 @@
             $("#txtRG").mask('00.000.000-0');
             $("#txtTel").mask('(00) 0000 0000');
             $("#txtCelular").mask('(00) 00000 0000');
+            $("#txtNIS").mask('000.00000.00-0');
         });
     </script>
     <script>
@@ -209,11 +219,13 @@
         {
             if(this.checked) 
             {
-                $('#btnProtetor').prop('disabled', false);
+                $('#btnProtetorUpload').prop('disabled', false);
+                $('#labelProtetor').removeClass("disabled");
             } 
             else 
             {
-                $('#btnProtetor').prop('disabled', true);
+                $('#btnProtetorUpload').prop('disabled', true);
+                $('#labelProtetor').addClass("disabled");
             }
         });
     </script>
