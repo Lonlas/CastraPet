124e840587d44f0f6af8235d33336f057f0a7c57
Nicolas Longo <nicolaslongo18@gmail.com>
2022-06-11 16:40:54 -0300

    Texto na home colocado

--- view/home.php
+++ view/home.php
@@ -18,24 +18,140 @@
                         <div class="row ">
                             <div class="col-md-6">
                                 <div class="p-sm-3 p-md-3 p-lg-4 p-3 bg-white">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus mi, venenatis et scelerisque a, consectetur eu neque. Cras id rhoncus tortor, eget     aliquam erat. Aliquam erat volutpat. Pellentesque vehicula quam quis elementum maximus. Suspendisse ut congue nibh. Quisque vitae imperdiet nulla, eget pellentesque orci. In nunc mauris, tristique ut ex nec, volutpat pretium elit. Etiam sollicitudin ullamcorper elit in ultricies.
-
-                                    Donec cursus placerat commodo. Duis luctus arcu enim. Etiam lobortis fermentum aliquet. In lectus velit, aliquet tempor rhoncus et, efficitur sit amet nibh. Nulla quis odio id nulla rutrum pretium. Maecenas consequat consequat posuere. Nullam sed mattis sapien.
-
-                                    Suspendisse vitae magna sed neque vestibulum fermentum. Quisque a vulputate sapien. Pellentesque condimentum scelerisque feugiat. Pellentesque luctus quam orci, ut aliquam lorem iaculis sed. Morbi ac metus ac nisl fermentum volutpat eu in nulla. Curabitur congue quam et urna porttitor condimentum. Aliquam lorem erat, euismod vel velit a, imperdiet luctus magna. Suspendisse aliquam sapien non nisi eleifend, et suscipit ante auctor. Vivamus rhoncus enim nulla, in dignissim sem elementum ut. Phasellus feugiat volutpat tempor. Sed tristique enim nec elit ornare varius. Mauris congue mauris vitae dui elementum vulputate. Suspendisse consequat, tellus nec tincidunt pellentesque, nisl augue fringilla odio, eget egestas nunc dui vel eros.
-
-                                    Donec pharetra lacinia posuere. Integer lobortis dui vel suscipit ullamcorper. Morbi eget cursus ipsum, commodo mollis magna. Aliquam eu suscipit mi. Phasellus eu scelerisque est. Phasellus ac convallis leo. Integer maximus suscipit laoreet. Suspendisse non justo posuere nisi hendrerit efficitur. Nullam tortor justo, porttitor nec neque vel, lacinia commodo ipsum.
+                                    <p>
+                                        Seja bem-vindo(a) ao projeto PetCastra, um site em que você pode se cadastrar para agendar uma castração gratuita para seu gato ou cachorro 
+                                        através do programa gratuito da Prefeitura de Franco da Rocha. Você sabe sobre as vantagens de castrar seu animalzinho? Algumas delas são:
+                                        <ul>
+                                            <li>Redução dos passeios externos e problemas associados a esta prática, como o desenvolvimento de doenças infecciosas contraídas nas ruas, traumas e atropelamentos, intoxicações e maus tratos;</li>
+                                            <li>Redução de doenças zoonóticas (transmitidas de pets para humanos), como a <span class="fw-bold text-decoration-underline">Raiva</span> e a <span class="fw-bold text-decoration-underline">Leishmaniose</span>;</li>
+                                            <li>Redução no <span class="fw-bold text-decoration-underline">comportamento de cio</span> e prevenção de doenças reprodutivas em fêmeas;</li>
+                                            <li>Redução do risco de enfermidades mamárias, uterinas e ovarianas como <span class="fw-bold text-decoration-underline">neoplasias</span> e piometra (infecção no útero);</li>
+                                            <li>Prevenção de doenças como pseudociese (<span class="fw-bold text-decoration-underline">gravidez psicológica</span>), hipertrofia mamária felina e estro persistente (cio);</li>
+                                            <li>Prevenção de <span class="fw-bold text-decoration-underline">distúrbios testiculares</span> em machos (neoplasias, orquites (inflamação do testículo), epididimites (inflamação do epidídimo), além de auxiliar no tratamento de doenças andrógeno-dependentes como a hiperplasia prostática benigna, prostatite crônica (inflamação da próstata), adenoma e hérnia perineal;</li>
+                                            <li>Auxílio no tratamento de doenças não relacionadas ao sistema reprodutivo, como diabetes e epilepsia;</li>
+                                            <li>Possível redução de comportamentos indesejados como territorialismo, agressividade e marcação de território.</li>
+                                        </ul>
+                                    </p>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="p-sm-3 p-md-3 p-lg-4 p-3 bg-white">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus mi, venenatis et scelerisque a, consectetur eu neque. Cras id rhoncus tortor, eget     aliquam erat. Aliquam erat volutpat. Pellentesque vehicula quam quis elementum maximus. Suspendisse ut congue nibh. Quisque vitae imperdiet nulla, eget pellentesque orci. In nunc mauris, tristique ut ex nec, volutpat pretium elit. Etiam sollicitudin ullamcorper elit in ultricies.
+                                    <div class="card">
+                                        <h4 class="card-header">Perguntas Frequentes:</h4>
+                                        <ol class="list-group list-group-flush">
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta1" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">É necessário pagar algo para castrar meu animal?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta1">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Não, o programa de castração é gratuito, o tutor tem apenas que lidar com os custos de transporte e cuidados pós-cirúrgicos.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Posso castrar mais de um animal?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta2">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> As vagas são limitadas e distribuídas para os moradores de Franco da Rocha baseado nos seguintes critérios:
+                                                        <br><br>
+                                                        Caso seja <span class="fw-bold">Protetor de Animais</span>: Têm direito a <span class="fw-bold">5</span> vagas para castração por mês;
+                                                        <br>
+                                                        Caso tenha <span class="fw-bold">Benefício Social</span>: Têm direito a <span class="fw-bold">2</span> vagas para castração por mês;
+                                                        <br>
+                                                        Para o restante da <span class="fw-bold">população</span>: Têm direito a apenas <span class="fw-bold">1</span> vaga para castração por mês;
+                                                    </p>
+                                                </div>
+                                            </div>
 
-                                    Donec cursus placerat commodo. Duis luctus arcu enim. Etiam lobortis fermentum aliquet. In lectus velit, aliquet tempor rhoncus et, efficitur sit amet nibh. Nulla quis odio id nulla rutrum pretium. Maecenas consequat consequat posuere. Nullam sed mattis sapien.
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">O programa castra outros animais além de gatos e cachorros?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta3">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Não, o programa castra apenas <span class="fw-bold">gatos</span> e <span class="fw-bold">cachorros</span>.
+                                                    </p>
+                                                </div>
+                                            </div>
 
-                                    Suspendisse vitae magna sed neque vestibulum fermentum. Quisque a vulputate sapien. Pellentesque condimentum scelerisque feugiat. Pellentesque luctus quam orci, ut aliquam lorem iaculis sed. Morbi ac metus ac nisl fermentum volutpat eu in nulla. Curabitur congue quam et urna porttitor condimentum. Aliquam lorem erat, euismod vel velit a, imperdiet luctus magna. Suspendisse aliquam sapien non nisi eleifend, et suscipit ante auctor. Vivamus rhoncus enim nulla, in dignissim sem elementum ut. Phasellus feugiat volutpat tempor. Sed tristique enim nec elit ornare varius. Mauris congue mauris vitae dui elementum vulputate. Suspendisse consequat, tellus nec tincidunt pellentesque, nisl augue fringilla odio, eget egestas nunc dui vel eros.
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta4" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Como faço para solicitar uma castração para meu animal?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta4">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Clique no botão “Login” no menu do site, faça login caso já possua um cadastro, caso contrário clique em “Não possuo uma conta” e preencha as informações para se cadastrar. Após ter feito o login, vá em “Meus Animais” e cadastre seu animal, depois clique em “Solicitar Castração” e então sua solicitação estará em análise. Após ser aprovada, informações da clínica e horário serão enviadas ao e-mail cadastrado junto com as orientações que devem ser seguidas antes de ocorrer a cirurgia.
+                                                    </p>
+                                                </div>
+                                            </div>
 
-                                    Donec pharetra lacinia posuere. Integer lobortis dui vel suscipit ullamcorper. Morbi eget cursus ipsum, commodo mollis magna. Aliquam eu suscipit mi. Phasellus eu scelerisque est. Phasellus ac convallis leo. Integer maximus suscipit laoreet. Suspendisse non justo posuere nisi hendrerit efficitur. Nullam tortor justo, porttitor nec neque vel, lacinia commodo ipsum.
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta5" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Por onde eu vou receber as informações da castração?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta5">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> As informações de qual clínica será a responsável por realizar a castração e o endereço junto com o horário serão enviados para o e-mail que foi usado na hora de cadastrar sua conta.
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta6" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Como funciona o processo de castração?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta6">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> A castração é um processo simples onde o animal, após ser anestesiado, tem seus órgãos reprodutivos removidos, sendo eles no caso dos machos: os testículos, e no caso das fêmeas: o útero e os ovários. Depois da cirurgia ser feita, é orientado ao tutor os cuidados e remédios que devem ser dados ao animal para uma recuperação rápida.
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta7" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Onde será castrado meu animal?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta7">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Seu animal será castrado em uma das clínicas que têm parceria com a prefeitura de Franco da Rocha.
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta8" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Tem riscos de acontecer algo com meu animal durante a cirurgia?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta8">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Sim, como toda cirurgia, há riscos de acontecer algo durante o processo cirúrgico. Contudo a cirurgia de castração é simples e rotineira, sendo a raro o animal ter sequelas graves após a cirurgia.
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <a class="list-group-item" data-bs-toggle="collapse" href="#pergunta9" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                                <li class="ms-2">Tem algo que impeça o meu animal de ser castrado?</li>
+                                            </a>
+                                            <div class="collapse" id="pergunta9">
+                                                <div class="card card-body border-primary">
+                                                    <p>
+                                                        <span class="fw-bold">R:</span> Sim, não poderá ser castrado caso o animal:
+                                                        <br>
+                                                        <ul>
+                                                            <li>Esteja no período de acasalamento;</li>
+                                                            <li>Não tenha seguido as orientações pré-cirúrgicas;</li>
+                                                            <li>Tenha alergia a anestesia;</li>
+                                                            <li>Esteja com filhotes no útero;</li>
+                                                        </ul>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </ol>
+                                    </div>
                                 </div>
                             </div>
                         </div>
--- view/homeClinica.php
+++ view/homeClinica.php
@@ -15,15 +15,21 @@
             <div class="row h-100 align-items-center">
                 <div class="p-3">
                     <div class="container p-sm-3 p-md-3 p-lg-4 p-3 bg-white">   
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus mi, venenatis et scelerisque a, consectetur eu neque. Cras id rhoncus tortor, eget     aliquam erat. Aliquam erat volutpat. Pellentesque vehicula quam quis elementum maximus. Suspendisse ut congue nibh. Quisque vitae imperdiet nulla, eget pellentesque orci. In nunc mauris, tristique ut ex nec, volutpat pretium elit. Etiam sollicitudin ullamcorper elit in ultricies.
-
-                        Donec cursus placerat commodo. Duis luctus arcu enim. Etiam lobortis fermentum aliquet. In lectus velit, aliquet tempor rhoncus et, efficitur sit amet nibh. Nulla quis odio id nulla rutrum pretium. Maecenas consequat consequat posuere. Nullam sed mattis sapien.
-
-                        Suspendisse vitae magna sed neque vestibulum fermentum. Quisque a vulputate sapien. Pellentesque condimentum scelerisque feugiat. Pellentesque luctus quam orci, ut aliquam lorem iaculis sed. Morbi ac metus ac nisl fermentum volutpat eu in nulla. Curabitur congue quam et urna porttitor condimentum. Aliquam lorem erat, euismod vel velit a, imperdiet luctus magna. Suspendisse aliquam sapien non nisi eleifend, et suscipit ante auctor. Vivamus rhoncus enim nulla, in dignissim sem elementum ut. Phasellus feugiat volutpat tempor. Sed tristique enim nec elit ornare varius. Mauris congue mauris vitae dui elementum vulputate. Suspendisse consequat, tellus nec tincidunt pellentesque, nisl augue fringilla odio, eget egestas nunc dui vel eros.
-
-                        Donec pharetra lacinia posuere. Integer lobortis dui vel suscipit ullamcorper. Morbi eget cursus ipsum, commodo mollis magna. Aliquam eu suscipit mi. Phasellus eu scelerisque est. Phasellus ac convallis leo. Integer maximus suscipit laoreet. Suspendisse non justo posuere nisi hendrerit efficitur. Nullam tortor justo, porttitor nec neque vel, lacinia commodo ipsum.
-
-                        Phasellus dictum dolor sit amet ultrices blandit. Phasellus iaculis tempor pharetra. Pellentesque facilisis eget tellus vel interdum. Morbi tincidunt mi eget mi finibus commodo. Maecenas sit amet rutrum lorem. Sed in sem vel ante molestie luctus. Vivamus aliquam turpis id ante dignissim, et congue sapien aliquam. Mauris quis varius mi, id varius velit. Maecenas consequat non diam vel scelerisque. Suspendisse potenti. Fusce purus massa, porta eget odio eu, hendrerit semper nisi. In risus quam, molestie at porta nec, aliquet eget felis. Duis ultricies venenatis justo id sollicitudin. Mauris non convallis lorem.
+                        <p>
+                            <h4>Bem-vindo(a) <?php echo $_SESSION["dadosLogin"]->nome;?> !</h4>
+                            <br>
+                            A clínica poderá acessar no menu uma lista de castrações designadas e aprovadas pelo administrador para definir um dia e horário para sua realização.
+                            Também há uma consulta de castrações que já têm seu dia e horário definidos, nessa tela a clínica deverá atualizar os <span class="fw-bold">status</span> da castração e colocar uma 
+                            <span class="fw-bold">observação</span>, caso seja necessário, conforme as seguintes situações:
+                            <br><br>   
+                            <ul>
+                                <li>“Animal Castrado”, caso tudo tenha ocorrido bem e o cirurgia foi bem sucedida;</li>
+                                <li>“Tutor não compareceu”, caso o tutor não apareça no dia e horário marcado;</li>
+                                <li>“Castração cancelada”, caso algum problema grave tenha acontecido antes da cirurgia e a castração tenha que ser cancelada;</li>
+                                <li>“Castração remarcada”, caso algum problema tenha acontecido antes da cirurgia e a castração tenha que ser remarcada para um outro dia;</li>
+                                <li>“Óbito”, caso o animal tenha falecido durante a cirurgia.</li>
+                            </ul>
+                        </p>
                     </div>
                 </div>
             </div>
